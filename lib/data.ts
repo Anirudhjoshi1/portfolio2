2a36f9d71e4525d6d7c89fa0ebc2cf146b5afb72
Anirudh Joshi <anirudh.53.aj@gmail.com>
2025-05-02 22:50:56 +0530

    new

--- lib/data.ts
+++ lib/data.ts
@@ -1,14 +1,10 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
-import acepitch from '../public/acepitch.jpg'
-import dashboard from '../public/dashboard.jpg'
-import Quiz from '../public/gamify.jpg'
-import Ecom from '../public/Ecommerce.jpg'
+import acepitch from '@/public/acepitch.jpg'
+import dashboard from '@/public/dashboard.jpg'
+import Quiz from '@/public/gamify.jpg'
+import Ecom from '@/public/Ecommerce.jpg'
 
 
 export const links = [
