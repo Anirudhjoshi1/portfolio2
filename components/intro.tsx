2f85b1c53539aa4be4102c7a53ed517e93eefb19
Anirudh Joshi <anirudh.53.aj@gmail.com>
2026-01-08 13:26:33 +0530

    hero

--- components/intro.tsx
+++ components/intro.tsx
@@ -103,7 +103,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://www.linkedin.com/in/anirudh-joshi-774a19197/"
+          href="https://www.linkedin.com/in/anirudh-joshi-1869b8373/"
           target="_blank"
         >
           <BsLinkedin />
